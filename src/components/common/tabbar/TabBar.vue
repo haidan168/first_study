9438feb6d33417f0ef7776c476fbabb1725c6e2f
haidan168 <1531637406@qq.com>
2020-11-07 17:17:22 +0800

    创建目录

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tab-bar"
+       :style="tabBarStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBar',
+  props: {
+    tabBarColor: {
+      type: String,
+      default: '#eee'
+    }
+  },
+  computed: {
+    tabBarStyle () {
+      return { backgroundColor: this.tabBarColor };
+    }
+  }
+}
+</script>
+
+<style>
+.tab-bar {
+  display: flex;
+  /* background-color: #eee; */
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav-bar class="nav-bar"><span slot="center">首页</span></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+.nav-bar {
+  background: var(--color-tint);
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
